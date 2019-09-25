9fe39ba187331d78b2eb681dc3f5861f115a4c68
furusawa057 <sfurusawa057@gmail.com>
2019-09-25 10:02:21 +0900

    任意の行数と列数で九九表を作成するプログラム
    rangeの引数をいまだに間違いがちなので早く慣れたい

---
+++ kuku_2.py
@@ -0,0 +1,12 @@
+def main():
+    row = int(input('行数を入力してください：'))
+    column = int(input('列数を入力してください：'))
+
+    for i in range(1, row + 1):
+        for j in range(1, column + 1):
+            print(f'{i * j} ', end="")
+        print('\n')
+
+
+if __name__ == '__main__':
+    main()
